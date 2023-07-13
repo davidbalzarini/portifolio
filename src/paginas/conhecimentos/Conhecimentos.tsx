f959f4b000b8c5969a53f42b4bb44d2f07a37807
David Balzarini <davidpereira2302@gmail.com>
2023-07-13 18:27:50 -0300

    deploy

--- src/paginas/conhecimentos/Conhecimentos.tsx
+++ src/paginas/conhecimentos/Conhecimentos.tsx
@@ -1,77 +1,37 @@
 import './Conhecimentos.css'
+import { useState } from 'react';
 
 
 export function Conhecimentos(){
 
+    const [displayText, setDisplayText] = useState('*passe o moouse por cima de alguma habilidade*');
 
-    // const elemento1 = document.getElementById('elemento1') as HTMLElement;
-    // const js = document.getElementById('js') as HTMLElement;
-    // const ts = document.getElementById('ts') as HTMLElement;
-    // const node = document.getElementById('node') as HTMLElement;
-    // const react = document.getElementById('react') as HTMLElement;
-    // const nest = document.getElementById('nest') as HTMLElement;
-    // const nest = document.getElementById('react') as HTMLElement;
-    
-    // js.addEventListener('mouseover', function() {
-    //   elemento1.textContent = 'javascript é';
-    // });
-    
-    // js.addEventListener('mouseout', function() {
-    //     elemento1.textContent = '*passe o mouse por cima de alguma habilidade*';
-    //   });
-    
-    // ts.addEventListener('mouseover', function() {
-    //   elemento1.textContent = 'typescript';
-    // });
-    
-    // ts.addEventListener('mouseout', function() {
-    //     elemento1.textContent = '*passe o mouse por cima de alguma habilidade*';
-    //   });
-    
-    //   node.addEventListener('mouseover', function() {
-    //   elemento1.textContent = 'node';
-    // });
-    
-    // node.addEventListener('mouseout', function() {
-    //     elemento1.textContent = '*passe o mouse por cima de alguma habilidade*';
-    //   });
-      
-    //   react.addEventListener('mouseover', function() {
-    //   elemento1.textContent = 'react';
-    // });
+    const handleMouseOverJs = () => {
+        setDisplayText('js');
+    };
 
-    // react.addEventListener('mouseout', function() {
-    //     elemento1.textContent = '*passe o mouse por cima de alguma habilidade*';
-    //   });
-    
-    
-    
-    
-    //   nest.addEventListener('mouseover', function() {
-    //   elemento1.textContent = 'nest';
-    // });
+    const handleMouseOverTs = () => {
+        setDisplayText('O typeScript é o Javascript tipado')
+    }
 
-    // nest.addEventListener('mouseout', function() {
-    //     elemento1.textContent = '*passe o mouse por cima de alguma habilidade*';
-    //   });
-    
-    
-    
-    
-    //   nest.addEventListener('mouseover', function() {
-    //   elemento1.textContent = 'nest';
-    // });
+    const handleMouseOverNode = () => {
+        setDisplayText('node')
+    }
 
-    // nest.addEventListener('mouseout', function() {
-    //     elemento1.textContent = '*passe o mouse por cima de alguma habilidade*';
-    //   });
+    const handleMouseOverReact = () => {
+        setDisplayText('React é sensacional para frontend')
+    }
 
-    
+    const handleMouseOverNest = () => {
+        setDisplayText('nest serve para backend')
+    }
+
+    const handleMouseOut = () => {
+        setDisplayText('*passe o moouse por cima de alguma habilidade*');
+    };
 
 
-
-
-
+    
 
 
     return(
@@ -81,14 +41,14 @@ export function Conhecimentos(){
             </div>
             <div className='containerc'>
                 <div>
-                    <p id="elemento1">*passe o mouse por cima de alguma habilidade*</p>
+                    <p id="elemento1">{displayText}</p>
                 </div>
                 <div className='gridhab'>
-                    <div id="js"><img width="48" height="48" src="https://img.icons8.com/color/48/000000/javascript--v1.png" alt="javascript--v1"/></div>
-                    <div id="ts"><img width="48" height="48" src="https://img.icons8.com/color/48/typescript.png" alt="typescript"/></div>
-                    <div id='node'><img width="48" height="48" src="https://img.icons8.com/fluency/48/node-js.png" alt="node-js"/></div>
-                    <div id='react'><img width="30" height="30" src="https://img.icons8.com/offices/30/react.png" alt="react"/></div>
-                    <div id='nest'><img width="48" height="48" src="https://img.icons8.com/color/48/nestjs.png" alt="nestjs"/></div>
+                    <div onMouseOver={handleMouseOverJs} onMouseOut={handleMouseOut}><img width="48" height="48" src="https://img.icons8.com/color/48/000000/javascript--v1.png" alt="javascript--v1"/></div>
+                    <div onMouseOver={handleMouseOverTs} onMouseOut={handleMouseOut}><img width="48" height="48" src="https://img.icons8.com/color/48/typescript.png" alt="typescript"/></div>
+                    <div onMouseOver={handleMouseOverNode} onMouseOut={handleMouseOut}><img width="48" height="48" src="https://img.icons8.com/fluency/48/node-js.png" alt="node-js"/></div>
+                    <div onMouseOver={handleMouseOverReact} onMouseOut={handleMouseOut}><img width="30" height="30" src="https://img.icons8.com/offices/30/react.png" alt="react"/></div>
+                    <div onMouseOver={handleMouseOverNest} onMouseOut={handleMouseOut}><img width="48" height="48" src="https://img.icons8.com/color/48/nestjs.png" alt="nestjs"/></div>
                 </div>
             </div>
         </>
